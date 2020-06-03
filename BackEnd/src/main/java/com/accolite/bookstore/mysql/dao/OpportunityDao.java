fb03bdc3bcc9dc5ab5fedbd40d0fc634b89a43d7
harshdomadia <harsh.domadia@somaiya.edu>
2020-06-03 18:13:02 +0530

    Delete OpportunityDao.java

--- BackEnd/src/main/java/com/accolite/bookstore/mysql/dao/OpportunityDao.java
+++
@@ -1,7 +0,0 @@
-package com.accolite.bookstore.mysql.dao;
-import org.springframework.data.repository.CrudRepository;
-
-import com.accolite.bookstore.model.Opportunity;
-public interface OpportunityDao extends CrudRepository<Opportunity,Integer> {
-
-}
